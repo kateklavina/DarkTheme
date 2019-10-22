9687b089999f84dbdfab4869832c170454185848
Arkadijs Makarenko <arkadijs.makarenko@gmail.com>
2019-10-22 18:00:00 +0300

    Initial Commit

---
+++ DarkTheme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DarkTheme
+//
+//  Created by Arkadijs Makarenko on 22/10/2019.
+//  Copyright © 2019 Arkadijs Makarenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
